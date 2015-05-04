058e9b24e82b013a2e2abf3fc769ff6615946070
Onepiece <Onepiece@10.16.99.251>
2015-05-04 12:38:51 +0200

    oprettet create,delete,read,update views.

---
+++ src/cdio3_v1/views/ReadView.java
@@ -0,0 +1,7 @@
+package cdio3_v1.views;
+
+import com.google.gwt.user.client.ui.Composite;
+
+public class ReadView extends Composite {
+
+}
---
+++ src/cdio3_v1/views/UpdateView.java
@@ -0,0 +1,7 @@
+package cdio3_v1.views;
+
+import com.google.gwt.user.client.ui.Composite;
+
+public class UpdateView extends Composite {
+
+}
