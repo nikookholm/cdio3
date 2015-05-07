c23f8a1452b4a4498e49b13aa2f0880148459518
Onepiece <Onepiece@10.16.99.251>
2015-05-07 14:04:37 +0200

    færddiggøre createView, når parameterne er færdige i Operator

--- src/cdio3_v1/views/CreateView.java
+++ src/cdio3_v1/views/CreateView.java
@@ -1,14 +1,23 @@
 
 	package cdio3_v1.views;
 
-	import com.google.gwt.event.dom.client.KeyUpEvent;
-	import com.google.gwt.user.client.ui.Button;
-	import com.google.gwt.user.client.ui.Composite;
-	import com.google.gwt.user.client.ui.Label;
-	import com.google.gwt.user.client.ui.TextBox;
-	import com.google.gwt.user.client.ui.VerticalPanel;
-	import com.google.gwt.event.dom.client.KeyUpEvent;
-	import com.google.gwt.event.dom.client.KeyUpHandler;
+	import cdio3_v1.client.ClientSideImpl;
+import cdio3_v1.server.IOperatorDAO;
+import cdio3_v1.server.OperatorDAO;
+import cdio3_v1.shared.FieldVerifier;
+import cdio3_v1.shared.OperatorDTO;
+
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
 
 	public class CreateView extends Composite {
 
@@ -24,19 +33,23 @@
 		TextBox iniBox;
 		TextBox passwordBox;
 		TextBox cprBox;
-
-		Button okButton;
-		Button cancelButton;
-		Button editButton;
+		Button save = new Button("tilf\u00F8j");
 
 
 
-
-		public CreateView(){
+		public CreateView(final ClientSideImpl clientImpl){
 
 
 			createPanel = new VerticalPanel();
-
+			
+//			createPanel.setHeight("100px");
+//			HorizontalPanel namePanel = new HorizontalPanel();
+//			HorizontalPanel idPanel  = new HorizontalPanel();
+//			HorizontalPanel iniPanel  = new HorizontalPanel();
+//			HorizontalPanel passPanel  = new HorizontalPanel();
+//			HorizontalPanel cprPanel  = new HorizontalPanel();
+			
+			
 			initWidget(createPanel);
 
 			nameLabel= new Label("navn");
@@ -62,25 +75,57 @@
 
 			boolean userBox = false;
 			boolean passBox = false;
+			
+			// use unicode escape sequence \u00F8 for '�'
+			save = new Button("Tilf\u00F8j");
+			save.setEnabled(false);
+
+			save.addClickHandler(new ClickHandler() {
 
 			nameBox.addKeyUpHandler(new KeyUpHandler(){
 
 				@Override
 				public void onKeyUp(KeyUpEvent event) {
-					//
 					
-
+					IOperatorDAO createPerson = new OperatorDAO(nameBox.getText(), iniBox.getText(), cprBox.getText());
+					
+					
+					// skal tilgåes igennem admin, er ikke færdiglavet
+					 clientImpl.service.
+				}
+				
+				@Override
+				public void onSuccess(void result) {
+					Window.alert("Operator gemt i databasen");
+				}
+				
+				@Override
+				public void onFailure(Throwable caught) {
+					Window.alert("Server fejl" + caught.getMessage());
 				}
 			}
 					);
-
-
-
-
-
-
-
-
-		}
-
+			
+			nameBox.addKeyUpHandler(new KeyUpHandler() {
+				
+				@Override
+				public void onKeyUp(KeyUpEvent event) {
+					if (!FieldVerifier.isValidID(nameBox.getText())){
+						nameBox.setStyleName("gwt-TextBox-invalid");
+						userBox = false;
+					}
+					
+				else{
+					nameBox.removeStyleDependentName("gwt-TextBox-invalidEntry");
+					userBox= true;
+				}
+					
+			
+			private void checkFormValid() {
+				if (nameBox)
+					save.setEnabled(true);
+				else
+					save.setEnabled(false);
+			}
+			}
 	}
