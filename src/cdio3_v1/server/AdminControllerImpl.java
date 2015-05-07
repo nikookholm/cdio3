9f6b356a555f535546f95b5c3b1977aeb783581d
Nehfi <kim@aptget.dk>
2015-05-07 14:35:54 +0200

    Lavet login-metode

--- src/cdio3_v1/server/AdminControllerImpl.java
+++ src/cdio3_v1/server/AdminControllerImpl.java
@@ -3,22 +3,22 @@ package cdio3_v1.server;
 import cdio3_v1.client.AdminController;
 import cdio3_v1.shared.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-
-import divvie.OperatorDTO;
 
 @SuppressWarnings("serial")
 public class AdminControllerImpl extends RemoteServiceServlet implements AdminController{
 
 	IOperatorDAO iOpr;
 	OperatorDTO opr;
-
+	OperatorDAO_db db;
+	
 	private void showCreate() throws Exception{	
 	}
 
-	private void showRead(List<OperatorDTO> lsO) throws Exception{
+	private void showRead(List<OperatorDTO> lsOpr) throws Exception{
 	}
 
 	private void showUpdate(OperatorDTO opr) throws Exception{
@@ -37,27 +37,56 @@ public class AdminControllerImpl extends RemoteServiceServlet implements AdminCo
 		String ini 	= opr.getIni();
 		String cpr 	= opr.getCpr();
 		String pswd = opr.getPassword();
+		
 		boolean cnd = checkOperatorInfo(id, name, ini, cpr, pswd);
 
 		if(cnd == true){
-			OperatorDAO_db.createOperator(opr);
+			db.createOperator(opr);
 		}
-
-
+		
+		
 	}
 
 	private void update(OperatorDTO oprD) throws Exception{
+		
+		int id 		= opr.getID();
+		String name = opr.getName();
+		String ini 	= opr.getIni();
+		String cpr 	= opr.getCpr();
+		String pswd = opr.getPassword();
+		
+		boolean cnd = checkOperatorInfo(id, name, ini, cpr, pswd);
 
+		if(cnd == true){
+			db.createOperator(opr);
+		}
 	}
 
 	private void delete(int ID) throws Exception{
-
+		
+		int id = opr.getID();
+		
+		db.deleteOperator(id);
 	}
 
-//	private boolean login(loginData lgD) throws Exception{
-//	}
+	private boolean login(String idAndPswd) throws Exception{
+		
+		boolean cond = false;
+		
+		int id = Integer.parseInt(idAndPswd.substring(0, idAndPswd.indexOf(","))); 
+		String pswd = idAndPswd.substring(idAndPswd.indexOf(",")+1);		
+		
+		OperatorDTO opr = db.getOperator(id);
+		
+		if(pswd == opr.getPassword()){
+			cond = true;
+		}
+		
+		return cond;
+	}
 
 	private boolean checkOperatorInfo(int id, String name, String ini, String cpr, String pswd){
+		
 		boolean cond 	= true;
 		int idC			= id;
 		String nameC 	= name;
