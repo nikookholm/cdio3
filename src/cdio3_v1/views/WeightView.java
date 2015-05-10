4330cf533640497682e4f57cf053ec5292d173c9
amal q <zizja4ka@yahoo.dk>
2015-05-10 14:42:32 +0200

    sletet ButtonView klassen og flyttet alle metodet i WeightView klassen

--- src/cdio3_v1/views/WeightView.java
+++ src/cdio3_v1/views/WeightView.java
@@ -1,9 +1,12 @@
 package cdio3_v1.views;
 
 
+
 import cdio3_v1.client.ClientSideImpl;
 import cdio3_v1.server.WeightFunctions;
 
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
@@ -15,52 +18,141 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 
 public class WeightView  extends Composite{
-	private VerticalPanel vPanel       = new VerticalPanel();
-	private VerticalPanel contentPanel;
-	Label textLabel = new Label();
-	
+	private VerticalPanel vPanel = new VerticalPanel();
+	private HorizontalPanel hPanel = new HorizontalPanel();
 
-	public WeightView(){
+	Button tara, readWeight, sendMessage, RM20;
+
+	public WeightView(final ClientSideImpl clientImpl){
 
 		initWidget(this.vPanel);
-		this.vPanel.setBorderWidth(1);		
-		
-		WeightsButtons weightsButtons = new WeightsButtons(this);
-		
-		this.vPanel.add(weightsButtons);
-		
+		this.vPanel.setBorderWidth(0);		
+
 		Anchor showWeigthFunction= new Anchor ("Vægt funktioner: vælg og tryk ");
 		vPanel.add(showWeigthFunction);
-		
-		this.contentPanel = new VerticalPanel();
-		this.vPanel.add(contentPanel);
-		
-		this.contentPanel.add(textLabel);
 
-		this.vPanel.add(textLabel);
+//******************************************tara knappen******************************************************
+		tara = new Button("Tara");
+		this.vPanel.add(tara);
+
+		tara.addClickHandler(new ClickHandler(){
+			public void onClick(ClickEvent event){
+				try {
+					clientImpl.service.tara(new AsyncCallback<Void>() {
+
+						@Override
+						public void onSuccess(Void result) {
+							Window.alert("Person gemt i kartotek");
+						}
+						@Override
+						public void onFailure(Throwable caught) {
+							Window.alert("Server fejl!" + caught.getMessage());
+						}				
+
+					});
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+//##############################################################################################################
+
+//******************************************VisInfo knappen***************************************************
 		
+		readWeight = new Button("VisInfo");
+		this.vPanel.add(readWeight);
 
+		readWeight.addClickHandler(new ClickHandler(){
+			public void onClick(ClickEvent event){
+				try {
+					clientImpl.service.getWeight(new AsyncCallback<Double>() {
 
+						public void onSuccess(Void result) {
+							Window.alert("Person gemt i kartotek");
+						}
+						@Override
+						public void onFailure(Throwable caught) {
+							Window.alert("Server fejl!" + caught.getMessage());
+						}
+						@Override
+						public void onSuccess(Double result) {
+														
+						}				
+
+					});
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+//############################################################################################################
+
+//**************************************************send besked***********************************************
+		
+		sendMessage = new Button("sendBesked");
+		this.vPanel.add(sendMessage);
+
+		sendMessage.addClickHandler(new ClickHandler(){
+			public void onClick(ClickEvent event){
+				try {
+					clientImpl.service.tara(new AsyncCallback<Void>() {
+
+						@Override
+						public void onSuccess(Void result) {
+							Window.alert("Person gemt i kartotek");
+						}
+						@Override
+						public void onFailure(Throwable caught) {
+							Window.alert("Server fejl!" + caught.getMessage());
+						}				
+
+					});
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+//***********************************************RM20***********************************************************
+		RM20= new Button("send besked for at få svar");
+		this.vPanel.add(RM20);
+
+		RM20.addClickHandler(new ClickHandler(){
+			public void onClick(ClickEvent event){
+				try {
+					clientImpl.service.RM20(null, new AsyncCallback<String>() {
+
+						public void onSuccess(Void result) {
+							Window.alert("Person gemt i kartotek");
+						}
+						@Override
+						public void onFailure(Throwable caught) {
+							Window.alert("Server fejl!" + caught.getMessage());
+						}
+						@Override
+						public void onSuccess(String result) {
+							// TODO Auto-generated method stub
+							
+						}				
+
+					});
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+//##############################################################################################################
+		
+		
+		
+		
 	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
