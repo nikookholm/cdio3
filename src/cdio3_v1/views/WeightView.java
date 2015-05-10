364668ac1c1e2d5ff616bb0c8a44790c28e1f6bc
amal q <zizja4ka@yahoo.dk>
2015-05-10 12:36:04 +0200

    weightView er ikke færdig

--- src/cdio3_v1/views/WeightView.java
+++ src/cdio3_v1/views/WeightView.java
@@ -1,6 +1,7 @@
 package cdio3_v1.views;
 
 
+import cdio3_v1.client.ClientSideImpl;
 import cdio3_v1.server.WeightFunctions;
 
 import com.google.gwt.user.client.ui.Anchor;
@@ -15,97 +16,35 @@ import com.google.gwt.event.dom.client.ClickHandler;
 
 public class WeightView  extends Composite{
 	private VerticalPanel vPanel       = new VerticalPanel();
-//	private VerticalPanel contentPanel;
+	private VerticalPanel contentPanel;
 	Label textLabel = new Label();
 	
-
 
 	public WeightView(){
 
 		initWidget(this.vPanel);
-		this.vPanel.setBorderWidth(1);
-//		this.vPanel.add(contentPanel);
-
+		this.vPanel.setBorderWidth(1);		
+		
+		WeightsButtons weightsButtons = new WeightsButtons(this);
+		
+		this.vPanel.add(weightsButtons);
+		
 		Anchor showWeigthFunction= new Anchor ("Vægt funktioner: vælg og tryk ");
-
 		vPanel.add(showWeigthFunction);
+		
+		this.contentPanel = new VerticalPanel();
+		this.vPanel.add(contentPanel);
+		
+		this.contentPanel.add(textLabel);
 
-		vPanel.add(new buttonsView());
-		
-		
 		this.vPanel.add(textLabel);
 		
 
 
 	}
-	public void runFunktion() throws Exception{
-		this.vPanel.clear();
-		WeightFunctions taraFunktion = new WeightFunctions();
-		Double tara = taraFunktion.tara();
-		
-		
-		textLabel = new Label(tara+"");
-		
-		this.vPanel.add(textLabel);
-				
-	}
-
-
-
+	
 }
-class buttonsView extends Composite{
-	private HorizontalPanel hPanel = new HorizontalPanel();
 
-	public buttonsView(){
-		initWidget(this.hPanel);
-//		this.hPanel.setBorderWidth(1);
-
-
-		Button tara = new Button("Tara");
-		this.hPanel.add(tara);
-		tara.addClickHandler(new ClickHandler(){
-			public void onClick(ClickEvent event){
-//				try {
-//					WeightFunctions weightFunktion = new WeightFunctions();
-//					weightFunktion.tara();
-//				} catch (Exception e) {	
-//				e.printStackTrace();
-//				}
-			
-
-
-			}
-		});
-
-
-		Button readWeigth = new Button("læs vægten");
-		this.hPanel.add(readWeigth);
-		readWeigth.addClickHandler(new ClickHandler(){
-			@Override
-			public void onClick(ClickEvent event) {
-			}
-
-		});
-
-		Button sendMessage = new Button("Send Besked");
-		this.hPanel.add(sendMessage);
-		sendMessage.addClickHandler(new ClickHandler(){
-			@Override
-			public void onClick(ClickEvent event) {
-			}
-		});
-
-		Button sendRM20 = new Button("send besked for at får svar");
-		this.hPanel.add(sendRM20);
-		sendRM20.addClickHandler(new ClickHandler(){
-			public void onClick(ClickEvent event) {
-
-			}
-
-		});
-
-	}
-}
 
 
 
