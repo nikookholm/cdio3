c97d473688c17f2b0a1b1845f2c69d62a3cf9587
Nehfi <kim@aptget.dk>
2015-05-10 14:01:39 +0200

    Lavet testWeightFunctions

---
+++ test/cdio3_v1/testWeightFunctions.java
@@ -0,0 +1,54 @@
+package cdio3_v1;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import cdio3_v1.server.*;
+
+public class testWeightFunctions {
+	
+	@Test
+	public void testTara() throws Exception{
+		WeightFunctions weight = new WeightFunctions();
+		double actual = weight.tara();
+		boolean condition = false;
+		if(actual != 0){
+			condition = true;
+		}
+		assertTrue(condition);
+	}
+
+	@Test
+	public void testGetWeight() throws Exception{
+		WeightFunctions weight = new WeightFunctions();
+		double actual = weight.getWeight();
+		boolean condition = false;
+		if(actual != 0){
+			condition = true;
+		}
+		assertTrue(condition);
+	}
+	
+	@Test
+	public void testSendMessage() throws Exception{
+		WeightFunctions weight = new WeightFunctions();
+		boolean condition = weight.sendMessage("thisBeATest");
+		
+		assertTrue(condition);
+	}
+	
+	@Test
+	public void testSendRM20() throws Exception{
+		WeightFunctions weight = new WeightFunctions();
+		String str = weight.sendRM20("thisBeATest");
+		boolean condition;
+		if(str.isEmpty()){
+			condition = false;
+		}else{
+			condition = true;
+		}
+		
+		assertTrue(condition);
+	}
+}
